960764cb5489be49196105e061edeae995d541bf
nekrochan <rubus.saxatilis.99@gmail.com>
2024-10-28 13:14:40 +0300

    Added: row FounderController, empty other controllers and empty index.html

---
+++ src/main/java/org/example/controllers/FounderController.java
@@ -0,0 +1,20 @@
+package org.example.controllers;
+
+import org.example.service.dto.FounderDto;
+import org.example.service.interfaces.FounderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+public class FounderController {
+
+    @Autowired
+    private FounderService founderService;
+
+    @GetMapping("/founders")
+    public List<FounderDto> getFounders(){
+        return founderService.findAllFounders();
+    }
+}
