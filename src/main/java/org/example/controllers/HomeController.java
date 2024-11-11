81937e82ee388c8d77ed8fd2f03b36a86064558f
nekrochan <rubus.saxatilis.99@gmail.com>
2024-11-11 03:14:28 +0300

    Home controller added & fragment htmls filled

---
+++ src/main/java/org/example/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package org.example.controllers;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String homePage() {
+        return "index";
+    }
+
+}
