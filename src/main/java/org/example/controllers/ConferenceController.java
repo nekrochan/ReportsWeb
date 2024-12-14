621c5e5258e4fbc530fb0ec4d08c6a449ada2126
nekrochan <rubus.saxatilis.99@gmail.com>
2024-12-15 01:28:03 +0300

    проверить как работает от модератора и пользователя; ссылки detail info в конференциях и докладах выдают 404; добавить редактирование и удаление на странице деталей

--- src/main/java/org/example/controllers/ConferenceController.java
+++ src/main/java/org/example/controllers/ConferenceController.java
@@ -2,6 +2,8 @@ package org.example.controllers;
 
 import org.example.service.dto.ConferenceDto;
 import org.example.service.interfaces.ConferenceService;
+import org.example.service.interfaces.FounderService;
+import org.example.service.interfaces.HostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
@@ -16,13 +18,23 @@ public class ConferenceController {
 
     @Autowired
     private final ConferenceService conferenceService;
+    @Autowired
+    private final HostService hostService;
+    @Autowired
+    private final FounderService founderService;
 
-    public ConferenceController(ConferenceService conferenceService) {
+    public ConferenceController(ConferenceService conferenceService,
+                                HostService hostService,
+                                FounderService founderService) {
         this.conferenceService = conferenceService;
+        this.hostService = hostService;
+        this.founderService = founderService;
     }
 
     @GetMapping("/add")
-    public String addConference() {
+    public String addConference(Model model) {
+        model.addAttribute("hosts", hostService.findAllHosts());
+        model.addAttribute("founders", founderService.findAllFounders());
         return "conference-add";
     }
 
