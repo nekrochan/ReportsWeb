a325842fd9cb8905ca6309ddbc2972ed5183351e
nekrochan <rubus.saxatilis.99@gmail.com>
2025-01-16 19:03:48 +0300

    логи для конф контроллера

--- src/main/java/org/example/controllers/ConferenceController.java
+++ src/main/java/org/example/controllers/ConferenceController.java
@@ -1,9 +1,12 @@
 package org.example.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.example.service.dto.ConferenceDto;
 import org.example.service.interfaces.ConferenceService;
 import org.example.service.interfaces.FounderService;
 import org.example.service.interfaces.HostService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
@@ -12,6 +15,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+@Slf4j
 @Controller
 @RequestMapping("/conferences")
 public class ConferenceController {
@@ -22,6 +26,8 @@ public class ConferenceController {
     private final HostService hostService;
     @Autowired
     private final FounderService founderService;
+
+    private static final Logger log = LoggerFactory.getLogger(ConferenceController.class);
 
     public ConferenceController(ConferenceService conferenceService,
                                 HostService hostService,
@@ -35,6 +41,8 @@ public class ConferenceController {
     public String addConference(Model model) {
         model.addAttribute("hosts", hostService.findAllHosts());
         model.addAttribute("founders", founderService.findAllFounders());
+
+        log.info("Requested page conference-add");
         return "conference-add";
     }
 
@@ -50,10 +58,11 @@ public class ConferenceController {
             redirectAttributes.addFlashAttribute("conferenceModel", conferenceModel);
             redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.conferenceModel",
                     bindingResult);
+            log.info("Unable to add conference: ".concat(conferenceModel.getConfName()));
             return "redirect:/conferences/add";
         }
         conferenceService.addConference(conferenceModel);
-
+        log.info("Conference added successfully: ".concat(conferenceModel.getConfName()));
         return "redirect:/conferences/all";
     }
 
@@ -61,6 +70,7 @@ public class ConferenceController {
     public String showAllConferences(Model model) {
         model.addAttribute("allConferences", conferenceService.findAllConferences());
 
+        log.info("conference-all page opening requested");
         return "conference-all";
     }
 
@@ -68,6 +78,8 @@ public class ConferenceController {
     public String conferenceByName(@PathVariable("conference-name") String conferenceName, Model model) {
         model.addAttribute("conferenceByName", conferenceService.findConferenceByConfName(conferenceName));
         System.out.println("conferenceByName method called with name: " + conferenceName);
+
+        log.info("conference-by-name page opening requested with conference name: ".concat(conferenceName));
         return "conference-by-name";
     }
 
@@ -75,6 +87,7 @@ public class ConferenceController {
     public String deleteConference(@PathVariable("conference-name") String conferenceName) {
         conferenceService.deleteConference(conferenceName);
 
+        log.info("Deleted conference: ".concat(conferenceName));
         return "redirect:/conferences/all";
     }
     
